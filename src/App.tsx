94c4df4499396b5c20ef7f5a7bf1406d065a2edc
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-02-15 17:15:40 +0300

    fix: Added provider to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Todos } from './components/todos';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { store } from './redux/store';
 
 function App() {
   return (
     <div>
-      <Todos />
+      <Provider store={store}>
+        <Todos />
+      </Provider>
     </div>
   );
 }
