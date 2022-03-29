1645ff89698275902cef5d138bcb27f18da0d8eb
Ryan Njoroge <keddelyronjoz@gmail.com>
2022-03-29 17:09:18 +0300

    feat: Added basic chart with dummy data

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Chart from 'react-apexcharts'
 
 type Props = {}
 
+const chartData = {
+    options: {
+        chart: {
+            id: "basic-bar"
+        },
+        xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        }
+    },
+    series: [
+        {
+            name: "series-1",
+            data: [30, 40, 45, 50, 49, 60, 70, 91]
+        },
+        {
+            name: "series-1",
+            data: [32, 12, 13, 33, 29, 60, 3, 7]
+        }
+    ]
+}
+
 export default function Dashboard(props: Props) {
     return (
-        <div>dashboard</div>
+        <>
+            <Chart
+                options={chartData.options}
+                series={chartData.series}
+                type='line'
+                width={500}
+            />
+        </>
     )
 }
